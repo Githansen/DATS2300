425c37391da4cb28f598c438595ddd3696b176b2
githansen <yohanhansen97@gmail.com>
2021-08-23 08:55:27 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] stigende = {2,3,4,5,6,7,8,9,10,11};
+        int[] synkende = {11,10,9,8,7,6,5,4,3,2,1};
+        int[] tilfeldig ={2,5,3,67,11,4,9,6,7,32};
+        System.out.println(Arrays.toString(stigende));
+    }
+}
