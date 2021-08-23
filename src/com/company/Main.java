6b86419429893f74eb758675b75dea908f674997
githansen <yohanhansen97@gmail.com>
2021-08-23 09:57:46 +0200

    First comm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,8 @@ public class Main {
         int[] synkende = {11,10,9,8,7,6,5,4,3,2,1};
         int[] tilfeldig ={2,5,3,67,11,4,9,6,7,32};
         System.out.println(Arrays.toString(stigende));
+        System.out.println(Uke1sortering.maks(tilfeldig));
+        System.out.println(Uke1sortering.min(tilfeldig));
+
     }
 }
--- src/com/company/Uke1sortering.java
+++ src/com/company/Uke1sortering.java
@@ -1,5 +1,28 @@
 package com.company;
 
 public class Uke1sortering {
-
+    //returnerer indeks til største tall
+        public static int maks(int[] numbers){
+            int m = 0;
+            int maksverdi = numbers[0];
+            for(int i =1; i < numbers.length; i++){
+                if(numbers[i] > maksverdi){
+                    m = i;
+                    maksverdi = numbers[i];
+                }
+            }
+            return m;
+        }
+        //returnerer indeks til minste tall
+        public static int min(int[] numbers){
+            int m = 0;
+            int minverdi = numbers[0];
+            for(int i = 1; i < numbers.length; i++){
+                if(numbers[i] < minverdi){
+                m=i;
+                minverdi = numbers[i];
+                }
+            }
+            return m;
+        }
 }
